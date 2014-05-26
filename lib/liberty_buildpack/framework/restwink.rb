b68dc13fb0386b2350965d241f6007082f190492
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-05-26 11:05:32 +0530

    Update restwink.rb

--- lib/liberty_buildpack/framework/restwink.rb
+++ lib/liberty_buildpack/framework/restwink.rb
@@ -47,7 +47,9 @@ module LibertyBuildpack::Framework
     # @return [String] returns +spring-auto-reconfiguration-<version>+ if the application is a candidate for
     #                  auto-reconfiguration otherwise returns +nil+
     def detect
-    	return "apche-wink-1.4"
+      if Dir.glob(/WEB-INF/lib/'wink.*.jar')
+    	  return "apche-wink-1.4"
+    	end
     end
 
     # Downloads the Auto-reconfiguration JAR
